db000018467fbc8b2d796481e91cf94a41066f60
Chrooom <matt552@my.yorku.ca>
2023-01-15 21:26:55 -0500

    stuff
    apparently type "Joystick" doesn't exist :/, saw it being used in tutorials

--- Mobile Horse Racing/Assets/Scripts/Player/PlayerMovement.cs
+++ Mobile Horse Racing/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerMove : MonoBehaviour
+using UnityEngine.EventSystems;
+public class PlayerMovement : MonoBehaviour
 {
+   // public Joystick J;
+    
     // Start is called before the first frame update
     void Start()
     {
